2601be7903433ed2623669fd60021285796d8712
jeffsLM <jeffes.dev@gmail.com>
2022-07-24 17:41:13 -0300

    add: go to home in logo component

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,18 +1,25 @@
 
-import { FC,HtmlHTMLAttributes } from 'react';
+import { FC, HtmlHTMLAttributes } from 'react';
 import styles from './styles.module.scss'
+import { useRouter } from 'next/router'
 
 interface LogoProps extends HtmlHTMLAttributes<HTMLHeadingElement> {
 
-    variant: 'full' | 'partial' | 'inline' 
+    variant: 'full' | 'partial' | 'inline'
 }
 
 export const Logo: FC<LogoProps> = ({ variant, ...rest }) => {
+    const router = useRouter();
+
+    function handleGoToHomePage() {
+        router.push('/')
+    }
+
     return (
-        <div className={styles.logoContainer} data-variant={variant}>
+        <a className={styles.logoContainer} data-variant={variant} onClick={handleGoToHomePage}>
             <h1 {...rest}>REPOS.IO</h1>
-            {variant == 'full' && <h2>Sua busca. Seu repositório. Apenas para você.</h2>}
-        </div>
+            {variant != 'inline' && <h2>Sua busca. Seu repositório. Apenas para você.</h2>}
+        </a>
     );
 }
 
